a33c7591cd27e7f2bbc2ed6deb44bf43381f2fe1
Rodrigo Valle <rovapin@gmail.com>
2022-08-07 12:49:20 -0500

    implemented join queries

--- queries.sql
+++ queries.sql
@@ -8,8 +8,43 @@ FROM employees
 WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
 AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
 
-SELECT first_name, last_name
+SELECT emp_no, first_name, last_name
 INTO retirement_info
 FROM employees
 WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
 AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+SELECT departments.dept_name,
+    dept_manager.emp_no,
+    dept_manager.from_date,
+    dept_manager.to_date
+FROM departments
+INNER JOIN dept_manager
+ON departments.dept_no = dept_manager.dept_no;
+
+SELECT retirement_info.emp_no,
+    retirement_info.first_name,
+    retirement_info.last_name,
+    dept_emp.to_date
+FROM retirement_info
+LEFT JOIN dept_emp
+ON retirement_info.emp_no = dept_emp.emp_no;
+
+SELECT r.emp_no,
+    r.first_name,
+    r.last_name,
+    d.to_date
+FROM retirement_info as r
+LEFT JOIN dept_emp as d
+ON r.emp_no = d.emp_no;
+
+SELECT ri.emp_no,
+    ri.first_name,
+    ri.last_name,
+    de.to_date
+INTO current_emp
+FROM retirement_info as ri
+LEFT JOIN dept_emp as de
+ON ri.emp_no = de.emp_no
+WHERE de.to_date = ('9999-01-01');
+
